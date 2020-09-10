db32a0a2e1e2d11c31ca73449721e80296d4251c
Ritik Chauhan <49956465+RitikChauhan@users.noreply.github.com>
2020-09-11 02:08:53 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <span>Enter String Below:</span>
+    <br>
+    <textarea v-model="message" placeholder="You can add multiple lines"></textarea>
+    <p>{{myStr2}}</p>
+    <button @click="changeCase()">Change String Case</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+      message:"",
+      myStr2:""
+    }
+  },
+  methods :{
+    changeCase : function(){
+      this.myStr2 = this.message.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style>
+</style>
